406242053b6160db3346053b07b1340e2e428d27
Rujina <rujinaakter3486@gmail.com>
2024-09-05 10:27:35 +0600

    papon mara gese

---
+++ src/BankAccount.java
@@ -0,0 +1,30 @@
+public class BankAccount {
+
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
