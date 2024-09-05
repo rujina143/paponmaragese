73b123e12c83a815b30472ae9767e4012ba6174f
Rujina <rujinaakter3486@gmail.com>
2024-09-05 10:33:37 +0600

    fawfaw

---
+++ src/sdfghujk.java
@@ -0,0 +1,30 @@
+public class sdfghujk {
+
+    private double balance;
+
+    public sdfghujk(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
